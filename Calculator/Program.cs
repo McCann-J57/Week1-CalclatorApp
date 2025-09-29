4d57dea34b10a6aacc949231293c7617e28b222d
McCann-J57 <McCann-J57@ulster.ac.uk>
2025-09-29 02:48:51 +0100

    Implemented program code.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Variable names should be short yet meaningful.
+// The choice of a variable name should be memoriable.
+// Designed to indicate to the casual observer the intent of its use. 
+
+// The integer variable stores he first number
+// Ask the ser to input the first number
+Console.WriteLine("Type in the first number, and then press Enter key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Ask the user to type the second number.
+Console.WriteLine("Type the second number, and then press Enter");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//Perfom the calculation
+int result = firstNumber + secondNumber;
+
+//Output the answer to the console
+Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+
+
+
